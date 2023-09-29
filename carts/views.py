8fd732ebca12703b448a50c0dbc619638e7561da
Agustinmiquel <agumiquel@.com>
2023-09-29 01:09:53 -0300

    Session para el carrito

--- carts/views.py
+++ carts/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 
+from .models import Cart
 
 def cart(request):
+    
+    user = request.user if request.user.is_authenticated else None
+    cart_id = request.session.get('cart_id')
+    
+    if cart_id:
+       cart = Cart.objects.get(pk=cart_id)
+    else:
+        cart = Cart.objects.create(user=user)
+
+    request.session['cart_id'] = cart.id
+   
     return render(request,'carts/carts.html',{
         
-    })
+})
